1a84759be52c3a869af881fd7cc92f38cc59e5ad
Grisalle <grommp007@gmail.com>
2023-09-04 21:25:52 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector(".form");
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
+};
+
+formEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayEl = Number(this.elements.delay.value);
+  const stepEl = Number(this.elements.step.value);
+  const amountEl = Number(this.elements.amount.value);
+
+  for (let i = 0; i < amountEl; i++) {
+    const promisePosition = i + 1;
+    createPromise(promisePosition, delayEl + i * stepEl)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
-}
+});
